66b18792dadf4e86becae9136a0f5270f8d95b4d
Sam Watts <Sam.Watts@hpdlendscape.com>
2022-09-02 09:43:24 +0100

    Removed junk

--- _next/static/TkYCRGmI4v_vtodkZz7q0/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/764aca8e71ce9f87.css","static/chunks/pages/index-3fdaa3ffa6ddf2a9.js"],"/_error":["static/chunks/pages/_error-7397496ca01950b1.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
