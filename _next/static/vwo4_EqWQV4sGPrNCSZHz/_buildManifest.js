b303d7308a6651e87d67d52fee8f5177b5000073
Sam Watts <Sam.Watts@hpdlendscape.com>
2022-09-02 10:14:31 +0100

    add emojis, who doesn't love an emoji

---
+++ _next/static/vwo4_EqWQV4sGPrNCSZHz/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/966f0b33291f9412.css","static/chunks/pages/index-d74b3190c37a5f13.js"],"/_error":["static/chunks/pages/_error-7397496ca01950b1.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
