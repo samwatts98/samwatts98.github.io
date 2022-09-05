525071cc2c29025cc43c702c5a202a265a62505e
Sam Watts <Sam.Watts@hpdlendscape.com>
2022-09-05 09:09:32 +0100

    fix screen jump

--- _next/static/CFjtEKdhWj67V5Y6zP0pW/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/65005a6f808f03bc.css","static/chunks/pages/index-c7288e6f7cb62bbd.js"],"/_error":["static/chunks/pages/_error-7397496ca01950b1.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
