f75a28286843e1c2f88faf5434a2a3deefeecd1c
Sam Watts <Sam.Watts@hpdlendscape.com>
2022-09-06 12:25:02 +0100

    added NextJS and NodeJS, forgot those!

---
+++ _next/static/6Vh-cQt2P6TiO3XWQZtu5/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/c9a94881b4e88790.css","static/chunks/pages/index-1aa0cb92caaaed66.js"],"/_error":["static/chunks/pages/_error-7397496ca01950b1.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
